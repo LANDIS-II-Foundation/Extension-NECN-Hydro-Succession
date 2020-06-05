fc6567bf987df3c3553d38ce5388b77a35a55aff
rmscheller <rmscheller@gmail.com>
2020-06-05 10:06:18 -0400

    Created test for harvesting.  Harvesting completely failed.

--- testing/BiomassHarvest_DAMM/BatchFile.bat
+++ testing/BiomassHarvest_DAMM/BatchFile.bat
@@ -3,10 +3,7 @@ rem Batch File to Run a Scenario
 rem The 'rem' keyword indicates that this is a remark
 
 
-call landis-ii scenario_landscape.txt
-
-
-rem call landis-ii scenario-single-cell.txt
+call landis-ii-7 scenario.txt
 
 
 pause
